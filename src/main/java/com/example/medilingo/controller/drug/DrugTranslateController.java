d0630ffb10734bc7e82655a14b30ed2299a72b5e
siyeon02 <chrysapark@cau.ac.kr>
2026-01-23 17:02:53 +0900

    기존 컨트롤러에 AuthenticationPrincipal 적용

--- src/main/java/com/example/medilingo/controller/drug/DrugTranslateController.java
+++ src/main/java/com/example/medilingo/controller/drug/DrugTranslateController.java
@@ -3,11 +3,14 @@ package com.example.medilingo.controller.drug;
 import com.example.medilingo.controller.drug.request.DrugTranslateRequest;
 import com.example.medilingo.controller.drug.response.DrugTranslateResponse;
 import com.example.medilingo.controller.drug.response.LocalProductDto;
+import com.example.medilingo.domain.member.Member;
+import com.example.medilingo.security.entity.UserDetailsImpl;
 import com.example.medilingo.service.DrugTranslateService;
 import com.example.medilingo.util.ApiResult;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -20,7 +23,8 @@ public class DrugTranslateController {
     private final DrugTranslateService drugTranslateService;
 
     @PostMapping("/translate")
-    public ResponseEntity<ApiResult<DrugTranslateResponse>> translate(@RequestBody DrugTranslateRequest reqDto){
+    public ResponseEntity<ApiResult<DrugTranslateResponse>> translate(@AuthenticationPrincipal UserDetailsImpl userDetails, @RequestBody DrugTranslateRequest reqDto){
+        Member member = userDetails.getUser();
         DrugTranslateResponse result = drugTranslateService.translate(reqDto);
         return ResponseEntity.ok(ApiResult.success(result));
     }
