9e08974838142a82842a86474f09519b7c830e52
siyeon02 <chrysapark@cau.ac.kr>
2026-01-26 17:43:05 +0900

    기존 LLM(openAi)에서 Gemini 사용으로 수정

--- src/main/java/com/example/medilingo/openai/config/OpenAiConfig.java
+++ src/main/java/com/example/medilingo/openai/config/OpenAiConfig.java
@@ -1,22 +1,22 @@
-package com.example.medilingo.openai.config;
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.MediaType;
-import org.springframework.web.reactive.function.client.WebClient;
-
-@Configuration
-public class OpenAiConfig {
-    @Bean
-    public WebClient openAIWebClient(
-            @Value("${openai.api-key}") String apiKey
-    ) {
-        return WebClient.builder()
-                .baseUrl("https://api.openai.com/v1")
-                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
-                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-                .build();
-    }
-}
+//package com.example.medilingo.openai.config;
+//
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.MediaType;
+//import org.springframework.web.reactive.function.client.WebClient;
+//
+//@Configuration
+//public class OpenAiConfig {
+//    @Bean
+//    public WebClient openAIWebClient(
+//            @Value("${openai.api-key}") String apiKey
+//    ) {
+//        return WebClient.builder()
+//                .baseUrl("https://api.openai.com/v1")
+//                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+//                .defaultHeader(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+//                .build();
+//    }
+//}
